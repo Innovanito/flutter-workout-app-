1c80fd52674e54a01234397dee46d96eb6a79970
Innovanito <khg05311@naver.com>
2021-09-16 18:02:49 +0900

    Area of focuss units are making

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 import './colors.dart' as color;
 
 class Homepage extends StatefulWidget {
@@ -10,6 +12,20 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  _initData() {
+    DefaultAssetBundle.of(context)
+        .loadString("json/info.json")
+        .then((value) => {
+              // json.decode(value);
+            });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -175,28 +191,125 @@ class _HomepageState extends State<Homepage> {
             ),
             Container(
               height: 180,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [
-                  Colors.red.withOpacity(0.2),
-                  Colors.green.withOpacity(0.2)
-                ], begin: Alignment.bottomLeft, end: Alignment.centerRight),
-              ),
               width: MediaQuery.of(context).size.width,
               child: Stack(
                 children: [
                   Container(
                     width: MediaQuery.of(context).size.width,
+                    margin: const EdgeInsets.only(top: 30),
                     height: 120,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                            image: AssetImage("assets/card.jpg"),
+                            fit: BoxFit.fill),
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 40,
+                              offset: Offset(8, 10),
+                              color: color.AppColor.gradientSecond
+                                  .withOpacity(.3)),
+                          BoxShadow(
+                              blurRadius: 10,
+                              offset: Offset(-1, -5),
+                              color: color.AppColor.gradientSecond
+                                  .withOpacity(.3)),
+                        ]),
+                  ),
+                  Container(
+                    height: 200,
+                    width: MediaQuery.of(context).size.width,
+                    margin: const EdgeInsets.only(right: 200, bottom: 30),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(20),
                       image: DecorationImage(
-                          image: AssetImage("assets/card.jpg"),
-                          fit: BoxFit.fill),
+                        image: AssetImage("assets/figure.png"),
+                      ),
                     ),
-                  )
+                  ),
+                  Container(
+                    width: double.maxFinite,
+                    height: 100,
+                    margin: const EdgeInsets.only(left: 150, top: 50),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("You are doing great",
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: color.AppColor.homePageDetail)),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        RichText(
+                            text: TextSpan(
+                                text: "Keep it up\n",
+                                style: TextStyle(
+                                    color: color.AppColor.homePagePlanColor,
+                                    fontSize: 16),
+                                children: [
+                              TextSpan(text: "stick to your plan")
+                            ]))
+                      ],
+                    ),
+                  ),
                 ],
               ),
-            )
+            ),
+            Row(
+              children: [
+                Text(
+                  "Area of focus",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w500,
+                      color: color.AppColor.homePageTitle),
+                )
+              ],
+            ),
+            Expanded(
+                child: ListView.builder(
+                    itemCount: 4,
+                    itemBuilder: (_, i) {
+                      return Row(
+                        children: [
+                          Container(
+                            width: 200,
+                            height: 170,
+                            padding: EdgeInsets.only(bottom: 5),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(15),
+                                image: DecorationImage(
+                                    image: AssetImage("assets/ex1.png")),
+                                boxShadow: [
+                                  BoxShadow(
+                                      blurRadius: 3,
+                                      offset: Offset(5, 5),
+                                      color: color.AppColor.gradientSecond
+                                          .withOpacity(0.1)),
+                                  BoxShadow(
+                                      blurRadius: 3,
+                                      offset: Offset(-5, -5),
+                                      color: color.AppColor.gradientSecond
+                                          .withOpacity(0.1)),
+                                ]),
+                            child: Center(
+                                child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: Text(
+                                "glues",
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: color.AppColor.homePageDetail),
+                              ),
+                            )),
+                          )
+                        ],
+                      );
+                    }))
           ],
         ),
       ),
